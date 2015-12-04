6fecae53dd609077e50c21c662a0b723a2f5ab1a
Amir Mofasser <amir.mofasser@volvocars.com>
2015-12-04 21:27:47 +0100

    Implemented command line arguments with flag pkg

--- src/github.com/amimof/hello/hello.go
+++ src/github.com/amimof/hello/hello.go
@@ -1,19 +1,31 @@
 package main
 
+/**
+*
+* Author: Amir Mofasser <amir.mofasser@gmail.com>
+*	https://github.com/amimof
+*
+*/
+
 import (
 	// "github.com/cheggaaa/pb"
 	"github.com/amimof/logger"
 	"io/ioutil"
 	"os"
 	"regexp"
-	"strconv"
+	"flag"
+	"path"
 	// "time"
 )
 
-var prog string = os.Args[0]
-var mroot string = os.Args[1]
-var sroot string = os.Args[2]
-var target string = os.Args[3]
+var (
+	prog string
+	mroot string
+	sroot string
+	target string
+	loglevel int
+)
+
 var extensions string = "\\.(mkv|MKV|mp4|MP4|m4p|M4P|m4v|M4V|mpg|MPG|mpeg|MPEG|mp2|MP2|mpe|MPE|mpv|MPV|3gp|3GP|nsv|NSV|f4v|F4V|f4p|F4P|f4a|F4A|f4b|F4P|vob|VOB|avi|AVI|mov|MOV|wmv|WMV|asd|ASD|flv|FLV|ogv|OGV|ogg|OGG|qt|QT|yuv|YUV|rm|RM|rmvb|RMVB)"
 var spattern []string = []string{"(.*?)S(\\d{1,2})E(\\d{2})(.*)"+extensions,
 	"(.*?)s(\\d{1,2})e(\\d{2})(.*)"+extensions,
@@ -47,39 +59,37 @@ func isFile(path string) bool {
 	return false
 }
 
-// Prints program usage to the user
-func usage() string {
-	return *(&prog) + " <movies> <series> <target> <loglevel>"
-}
-
-// Checks wether a given filename is considered to be a movie
-func isMovie(filename string) (bool, error) {
-	log.Debug("Checking if " + filename + " is a movie")
-	for index, element := range mpattern {
+// Checks wether a given filename is considered to be a movie or series
+// based on the specified regexp patterns
+func isMedia(filename string, pattern []string) (bool, error) {
+	for index, element := range pattern {
+		_ = index
 		r, err := regexp.Compile(element)
 		if err == nil {
 			match := r.MatchString(filename)
 			if match {
-				log.Debug(index, match)
 				return true, nil
 			}
 		} else {
 			return false, err
 		}
 	}
-	log.Debug(filename, "did not match")
 	return false, nil
 }
 
 func main() {
 
+	// Read arguments
+	flag.StringVar(&mroot, "m", ".", "Directory to your movies. Current directory (.) by default")
+	flag.StringVar(&sroot, "s", ".", "Directory to your series. Current directory (.) by default")
+	flag.StringVar(&target, "t", ".", "Target directory. Typically your Downloads folder. Current directory (.) by default")
+	flag.IntVar(&loglevel, "v", 3, "Log level. 3=DEBUG, 2=WARN, 1=INFO, 0=DEBUG")
+	flag.Parse()
+
 	// Sets the loglevel.
 	// First we need to read from args and convert it to an int
-	loglevel, err := strconv.Atoi(os.Args[4])
-	if err != nil {
-		log.Error("Unexpected arguments")
-	}
 	log.Level.SetLevel(loglevel)
+	log.Debug("Log level is", loglevel)
 
 	// Check if movies root exists
 	if !exists(mroot) {
@@ -114,18 +124,32 @@ func main() {
 
 	// Main
 	var mmatches []string
+	var smatches []string
 	f, err := ioutil.ReadDir(target)
 	log.Debug("Looking in", target)
 	for i, file := range f {
-		if isM, errM := isMovie(file.Name()); isM {
+		log.Debug("Checking", file.Name())
+		if isM, errM := isMedia(file.Name(), mpattern); isM {
 			if errM == nil {
-				log.Debug("Is a movie", file.Name(), i, err)
+				log.Debug("Movie found", file.Name(), i, err)
 				mmatches = append(mmatches, file.Name())
+				srcf := path.Join(target, file.Name())
+				log.Info("Copying", srcf)
 			} else {
 				log.Error("Error", errM)
 			}
 		}
+		if isS, errS := isMedia(file.Name(), spattern); isS {
+			if errS == nil {
+				log.Debug("Serie found", file.Name(), i, err)
+				smatches = append(smatches, file.Name())
+			} else {
+				log.Error("Error", errS)
+			}
+		}
 	}
+
 	log.Info("Movies matched in dir is", len(mmatches))
+	log.Info("Series matched in dir is", len(smatches))
 
 }
